eff63531a960fe5193ec70609a1780216e08ea6d
Thomas Hawes <tchawes@gmail.com>
2023-01-25 16:38:23 -0500

    cleanup

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,6 @@
-# This example requires the 'message_content' intent.
 import os, sys, discord
 from discord.ext import commands
-from ksmdiscord import *
+from engine import *
 from io import StringIO
 
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,14 +12,30 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.command()
 async def ksm(ctx, realm, character):
+    # Setup a variable to capture stdout from print statements in function
     temp_out = StringIO()
     sys.stdout = temp_out
+    
+    # Calls function and creates output
     print("```")
-    ksm = my_function(realm, character)
+    ksm = ksm_status(realm, character)
     print("```")
+    
+    # Returns stdout to the system
     sys.stdout = sys.__stdout__
+    
+    # Post the output back to discord channel
     await ctx.send(temp_out.getvalue())
 
+@bot.command()
+async def rank(ctx, realm, character, role):
+    rank = rio_rank(realm, character, role)
+    if rank is None:
+        await ctx.send("Character is not that role.")
+    else:
+        await ctx.send(rank)
+
+# Error Handling
 @ksm.error
 async def ksm_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
@@ -28,4 +43,14 @@ async def ksm_error(ctx, error):
 
     elif isinstance(error, commands.CommandError):
         await ctx.send('Character not found... Please check the name and try again')
+
+# Error Handling
+@rank.error
+async def ksm_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Missing argument... Please ensure you use $rank realm character role(tank,dps,healer)')
+
+    elif isinstance(error, commands.CommandError):
+        await ctx.send('Character/realm/role combination not found... Please try again')
+
 bot.run(TOKEN)
---
+++ src/engine.py
@@ -0,0 +1,85 @@
+import requests, json, urllib
+from prettytable import PrettyTable
+
+def ksm_status(realm, character):
+    #Config
+    min_key_level = 13
+    io_req = 2000
+    dungeons = ["AA", "AV", "COS", "HOV", "NO", "RLP", "SBG", "TJS"]
+    
+    #URI encode Player name to account for special characters
+    encoded_name = urllib.parse.quote(character)
+
+    # Retrieve character information from Raider.io API
+    api_url = "https://raider.io/api/v1/characters/profile?region=us&realm={}&name={}&fields=gear%2Cguild%2Cmythic_plus_scores_by_season%3Acurrent%2Cmythic_plus_best_runs%2Cmythic_plus_alternate_runs".format(realm,encoded_name)
+    response = requests.get(api_url).json()
+
+    # Retrieve the character information
+    print("Character Information\n---------------------") 
+    #print(response["name"],"<",response["guild"]["name"],">")
+    print("{} <{}>".format(response["name"],response["guild"]["name"]))
+    print(response["gear"]["item_level_equipped"],response["active_spec_name"],response["class"])
+    print(response["realm"])
+    print(response["mythic_plus_scores_by_season"][0]["scores"]["all"], end = " ")
+    if response["mythic_plus_scores_by_season"][0]["scores"]["all"] < io_req:
+        print("- You need", io_req - round(response["mythic_plus_scores_by_season"][0]["scores"]["all"]), "more points.")
+    else:
+        print("( ✔ )")
+
+    # Function to search for Tyrannical Affix
+    def tyrannical(dungeon):
+        for best_runs in response["mythic_plus_best_runs"]:
+            if best_runs["short_name"] == dungeon and best_runs["affixes"][0]["name"] == "Tyrannical":
+                return best_runs["mythic_level"]
+            else:
+                for alt_runs in response["mythic_plus_alternate_runs"]:
+                    if alt_runs["short_name"] == dungeon and alt_runs["affixes"][0]["name"] == "Tyrannical":
+                        return alt_runs["mythic_level"]
+
+    # Function to search for Fortified Affix
+    def fortified(dungeon):
+        for best_runs in response["mythic_plus_best_runs"]:
+            if best_runs["short_name"] == dungeon and best_runs["affixes"][0]["name"] == "Fortified":
+                return best_runs["mythic_level"]
+            else:
+                for alt_runs in response["mythic_plus_alternate_runs"]:
+                    if alt_runs["short_name"] == dungeon and alt_runs["affixes"][0]["name"] == "Fortified":
+                        return alt_runs["mythic_level"]
+
+    # Setup the results table    
+    resultsTable = PrettyTable(["Dungeon","Tyrannical","Fortified"])
+
+    # Loop through the dungeon short names in array
+    for dungeon in dungeons:
+        result = tyrannical(dungeon)
+        if result is None:
+            bestTyrannical = "-"
+        elif result >= min_key_level:
+            bestTyrannical = "✔"
+        else:
+            bestTyrannical = result
+        
+        result = fortified(dungeon)
+        if result is None:
+            bestFortified = "-"
+        elif result >= min_key_level:
+            bestFortified = "✔"
+        else:
+            bestFortified = result
+        resultsTable.add_row([dungeon,bestTyrannical,bestFortified])
+    
+    # Output results in a "Pretty" table
+    print("")
+    print(resultsTable)
+    print("(Anything",min_key_level,"or higher shows as a ✔)\n")
+    print("Last Updated:",response["last_crawled_at"])
+
+def rio_rank(realm, character, role):
+    #URI encode Player name to account for special characters
+    encoded_name = urllib.parse.quote(character)
+
+    # Retrieve character information from Raider.io API
+    api_url = "https://raider.io/api/v1/characters/profile?region=us&realm={}&name={}&fields=mythic_plus_ranks".format(realm,encoded_name)
+    response = requests.get(api_url).json()
+    classRank = response["mythic_plus_ranks"]["class_" + str(role)]["realm"]
+    return str.capitalize(character)+" is the #"+str(classRank)+" "+response["active_spec_name"]+" "+response["class"]+" "+str.capitalize(role)+" on "+str.capitalize(realm)
